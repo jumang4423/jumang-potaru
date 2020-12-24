13fb95fd08dff4d589fb635b77264433b62ac856
jumang4423 <catontheskate4423@gmail.com>
2020-12-24 14:10:17 +0900

    yattaze

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+
 const resolve = require('path').resolve
 exports.onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
--- src/layouts/MainPage.tsx
+++ src/layouts/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery　} from "gatsby"
+import { graphql, StaticQuery, useStaticQuery } from "gatsby"
 import RotateJumang from "@/components/RotateJumang"
 import Header from "@/components/Header"
 import MDArea from "@/components/MDArea"
@@ -16,43 +16,45 @@ function isWebGLAvailable() {
         return false;
 
     }
-
 }
 
 //basically this <Apps /> for only jsx components
-export default function MainPage (data) {
-    const text : Object = JSON.stringify(data, null, 4)
-    
+export default function MainPage() {
+
     const MPPost = () => {
         return (
-            <div>
-                {text}
-            </div>
+            <StaticQuery
+                query={
+                    graphql`
+            query MyQuery {
+                allMicrocmsPotaruCms {
+                    nodes {
+                        title
+                        contents
+                    }
+                }
+            }`}
+                render={data => (
+                    <>
+                        <div dangerouslySetInnerHTML={{ __html: data.allMicrocmsPotaruCms.nodes[1].contents }} />
+                    </>
+                )}
+            />
         )
     }
 
     // const body : string = "<div>test</div>"
-    // const MPPost: React.FC<{ body: string }> = ({ body }) => (
+    // const MPPost: React.FC<{ body: {body}) => (
     //     <div dangerouslySetInnerHTML={{ __html: body }} />
     // )
     return (
         <div className="MainPage">
 
             <div>
-                { isWebGLAvailable() ? <RotateJumang /> : <p id="webglError"></p>}
+                {isWebGLAvailable() ? <RotateJumang /> : <p id="webglError"></p>}
             </div>
             <MDArea MPPost={MPPost} />
             <Header />
         </div>
     );
 };
-
-export const query = graphql`
-  {
-    allMicrocmsPotaruCms {
-      nodes {
-        title
-      }
-    }
-  }
-`
