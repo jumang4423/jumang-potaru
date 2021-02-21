a10a1683aba9acf58bfbc928ca5a688aaea0aa38
jumang4423 <catontheskate4423@gmail.com>
2021-02-21 18:20:02 +0900

    rotatejumang delay

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,22 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Router } from "@reach/router"
 import Header from "@/components/Header"
 import MainPage from "@/layouts/MainPage"
 import "@/styles/layout/Layout.scss"
 
 const RotateJumang = React.lazy(() =>
-    import("@/components/RotateJumang")
+  import("@/components/RotateJumang")
 )
 
 const Layout: React.FC<any> = () => {
   const isSSR = typeof window === "undefined"
+
+  const [isRotateJumang, setIsRotateJumang] = useState<Boolean>(false)
+  useEffect(() => {
+    setTimeout(() => { setIsRotateJumang(true) }, 500);
+  }, []);
+
   return (
     <>
       <Header />
       <div className="MainPage">
-        {!isSSR &&
+        {!isSSR && isRotateJumang &&
           <React.Suspense fallback={<div />}>
-            <RotateJumang />
+             <RotateJumang />
           </React.Suspense>
         }
         <Router>
