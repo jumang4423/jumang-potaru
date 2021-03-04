620d700e5ea2a5ce9e5b94669a099bdb752b0220
jumang4423 <catontheskate4423@gmail.com>
2021-03-04 14:45:32 +0900

    モルカーマスタリング

--- src/components/ConsoleLog.tsx
+++ src/components/ConsoleLog.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 
 const ConsoleLog: React.FC<Props> = () => {
-    console.log("%cany problems? ur pull request will help me => ", "background:grey; color: white; font-size:17px")
+    console.log("%cany problems? ur pull request will help me! => ", "background:grey; color: white; font-size:17px")
     console.log("%chttps://github.com/jumang4423/jumang-potaru", "font-size:17px")
     return (null)
 }
