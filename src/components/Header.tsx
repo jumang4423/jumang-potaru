98734580b1e576f845ac131014994d17398fb9c5
ryoma okuda <catontheskate4423@gmail.com>
2021-09-01 01:03:23 +0900

    new menu!

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeaderFoot from "@/components/HeaderFoot"
-import HeaderThings from "@/components/HeaderThings"
+import { motion } from "framer-motion";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { Menu, MenuItem } from "@material-ui/core"
+import { Link } from "gatsby"
 import "@/styles/component/Header.scss";
 
 interface Props {
 }
 
 const Header: React.FC<Props> = () => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
     return (
         <div className="Header">
             <HeaderFoot />
-            <HeaderThings />
+            {/* <HeaderThings /> */}
+            <motion.div
+                className="opaopa"
+                initial={
+                    {
+                        opacity: 0.0,
+                        backgroundColor: 'rgba(255, 100, 0, 0)',
+                        color: 'rgba(50, 50, 50, 0.9)'
+                    }}
+                animate={
+                    {
+                        opacity: 1,
+                        backgroundColor: 'rgba(200, 200, 200, 0.0)'
+                    }}
+                whileHover={
+                    {
+                        scale: 1.06,
+                        backgroundColor: 'rgba(200, 200, 200, 0.4)',
+                    }}
+                whileTap={
+                    {
+                        scale: 0.96,
+                        backgroundColor: 'rgba(230, 230, 200, 0.6)'
+                    }}
+                onClick={(event) => {
+                    setAnchorEl(event.currentTarget)
+                }}>
+                <ExpandMoreIcon />
+            </motion.div>
+
+            <Menu
+                id="simple-menu"
+                keepMounted
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}>
+
+                <Link to="/projects" className={"nodeco"}>
+                    <MenuItem onClick={() => {
+                        setAnchorEl(null)
+
+                    }}>
+                        <div className={"clear_font"}>
+                            projects
+                        </div>
+                    </MenuItem>
+                </Link>
+
+                <Link to="/about" className={"nodeco"}>
+                    <MenuItem onClick={() => {
+                        setAnchorEl(null)
+
+                    }}>
+                        <div className={"clear_font"}>
+                            about
+                        </div>
+                    </MenuItem>
+                </Link>
+
+                <Link to="/backwash" className={"nodeco"}>
+                    <MenuItem onClick={() => {
+                        setAnchorEl(null)
+                    }}>
+                        <div className={"clear_font"}>
+                            backwash
+                        </div>
+                    </MenuItem>
+                </Link>
+
+            </Menu>
+
         </div>
     )
 }
